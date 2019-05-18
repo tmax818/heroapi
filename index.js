ce5065a52ad8beb2d771ca6153c7eaf0fd692d0f
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-17 22:25:26 -0700

    add cors

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const path = require('path');
+const app = express();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const quotes = require('./quotes');
 
-express()
+app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.json(quotes))
-  .get('/:id', (req, res) => res.json(quotes[req.params.id]))
+  .get('/:id', cors(), (req, res) => res.json(quotes[req.params.id]))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
