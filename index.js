dbebafe2eef04fc3eba4ad9e87280da056a3ad3b
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-17 22:32:12 -0700

    yeah

--- index.js
+++ index.js
@@ -9,6 +9,6 @@ app
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.json(quotes))
+  .get('/', cors(), (req, res) => res.json(quotes))
   .get('/:id', cors(), (req, res) => res.json(quotes[req.params.id]))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
