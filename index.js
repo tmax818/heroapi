f3bfd068b248c109f39dacb9eeeafb795dcc040c
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-20 12:58:11 -0700

    activate route/api

--- index.js
+++ index.js
@@ -3,13 +3,16 @@ const path = require('path');
 const app = express();
 const cors = require('cors');
 const PORT = process.env.PORT || 5000;
-const quotes = require('./quotes');
+// const quotes = require('./quotes');
+const quotesRoute = require('./routes/api/quotes');
 
-app
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', cors(), (req, res) => res.render('pages/index'))
-  .get('/quotes', cors(), (req, res) => res.json(quotes))
-  .get('/quotes/:id', cors(), (req, res) => res.json(quotes[req.params.id]))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/api', quotesRoute);
+
+app.set('views', path.join(__dirname, 'views'));
+
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => res.render('pages/index'));
+
+app.listen(PORT, () => console.log(`Listening on ${PORT}`));
--- routes/api/quotes.js
+++ routes/api/quotes.js
@@ -1,2 +1,13 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
+const quoteslist = require('./quoteslist');
+const router = express.Router();
+
+router.get('/quotes', cors(), (req, res) => {
+  res.json(quoteslist);
+});
+
+router.get('/:id', cors(), (req, res) => res.json(quoteslist[req.params.id]));
+
+module.exports = router;
