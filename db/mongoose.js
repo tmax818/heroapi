cad39543e1ae2f09504b38b083f5d3da6683a5f1
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-21 10:07:32 -0700

    add dotenv and yeah.

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,14 +1,7 @@
 const mongoose = require('mongoose');
-mongoose.connect('mongodb://tmax818:reds1234@ds259596.mlab.com:59596/nata', {
+require('dotenv');
+
+mongoose.connect(`${process.env.DBURL}`, {
   useNewUrlParser: true,
   useCreateIndex: true
 });
-
-// const Todo = mongoose.model('Todo', {
-//   title: {
-//     type: String
-//   },
-//   complete: {
-//     type: Boolean
-//   }
-// });
--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const app = express();
+require('dotenv').config();
 require('./db/mongoose');
 
 const PORT = process.env.PORT || 5000;
