f2221d1de3f9e07ff57b8a247030227874a5058f
Tyler Maxwell <tylermaxwell661@gmail.com>
2019-05-21 10:21:11 -0700

    test again

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(`${process.env.DBURL}`, {
+mongoose.connect('mongodb://tmax818:reds1234@ds259596.mlab.com:59596/nata', {
   useNewUrlParser: true,
   useCreateIndex: true
 });
